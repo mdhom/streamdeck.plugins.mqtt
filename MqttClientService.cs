00d8ff42851313faadf63bf866785f08fc64e1bb
Max Dhom <md@adva.store>
2022-03-16 08:18:02 +0100

    lock subscribing, prohibit mulitple connections

--- MqttClientService.cs
+++ MqttClientService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -10,25 +9,31 @@ using MQTTnet.Client.Disconnecting;
 using MQTTnet.Client.Options;
 using MQTTnet.Client.Receiving;
 using MQTTnet.Extensions.ManagedClient;
-using SharpDeck.Connectivity;
 
 namespace streamdeck.plugins.mqtt
 {
     public class MqttClientService : IHostedService, IMqttApplicationMessageReceivedHandler, IMqttClientDisconnectedHandler
     {
+        private readonly SemaphoreSlim _syncRoot = new SemaphoreSlim(1, 1);
+        private string _broker;
+        private int _port;
+
         public IManagedMqttClient MqttClient { get; private set; }
         public HashSet<string> SubscriptionTopics { get; } = new HashSet<string>();
 
         public event EventHandler<MqttApplicationMessageReceivedEventArgs> MessageReceived;
         public event EventHandler Disconnected;
 
-        public MqttClientService(IStreamDeckConnection connection)
-        {
-
-        }
-
         public async Task Connect(string broker, int port)
         {
+            if (broker == _broker && port == _port)
+            {
+                // already connected / connecting to that target
+                return;
+            }
+            _broker = broker;
+            _port = port;
+
             if (MqttClient != null && MqttClient.IsConnected)
             {
                 Disconnect();
@@ -69,13 +74,22 @@ namespace streamdeck.plugins.mqtt
             return Task.CompletedTask;
         }
 
-        public Task SubscribeAsync(string topic)
+        public async Task SubscribeAsync(string topic)
         {
-            if (string.IsNullOrEmpty(topic)) return Task.CompletedTask;
+            try
+            {
+                // locking subscription handling because of multithreading concurrency
+                await _syncRoot.WaitAsync();
 
-            if (SubscriptionTopics.Add(topic))
-                return MqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(topic).Build());
-            return Task.CompletedTask;
+                if (string.IsNullOrEmpty(topic)) return;
+
+                if (SubscriptionTopics.Add(topic))
+                    await MqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(topic).Build());
+            }
+            finally
+            {
+                _syncRoot.Release();
+            }
         }
 
         public Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
